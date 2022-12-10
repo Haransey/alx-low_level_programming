ae1a184a621a70da7f3bfb0442f090437faeca01
Haransey <adeyemiyakub93@gmail.com>
2022-12-09 21:25:57 -0800

    ex04

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print the alphabet in lower case except e and q
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	
+	a = 97;
+	while (a < 123)
+	{
+		if (a != 101 && a != 113)
+			putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
