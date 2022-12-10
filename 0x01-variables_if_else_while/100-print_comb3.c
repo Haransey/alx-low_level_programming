93e92090b30d21e4492d3ee0453e8b7d0554ab0b
Haransey <adeyemiyakub93@gmail.com>
2022-12-09 22:43:01 -0800

    ex100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print the two digit numbers with comma and space
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	a = 0;
+	while (a < 100)
+	{
+		b = a / 10;
+		c = a % 10;
+		if ( c > b)
+		{
+			putchar(b + '0');
+			putchar(c + '0');
+			if (a < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
