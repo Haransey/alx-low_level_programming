08ce566cc3d861b36036a77803c4c215b8dda80b
Haransey <adeyemiyakub93@gmail.com>
2022-12-09 20:42:47 -0800

    ex02

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print the alphabet in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 97;
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return(0);
+}
