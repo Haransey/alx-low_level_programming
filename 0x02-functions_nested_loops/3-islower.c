fe5efe97b899bfb9d7990bfed5c8a3e8eb2cad7b
Haransey <adeyemiyakub93@gmail.com>
2022-12-13 07:52:40 -0800

    ex03
    git commit -m ex03

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - this checks lowercase
+ * Description- this is used to check the lowercase
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
+
+int _islower(int a)
+{
+	if (a >= 97  && a <= 122)
+		return (1);
+	else
+		return (0);
+}
