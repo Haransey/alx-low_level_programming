9e21b3d6ff40e06712ebfdadfe0d6f8275a6cc43
Haransey <adeyemiyakub93@gmail.com>
2022-12-20 05:37:41 -0800

    ex02

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * * @s: String to count
+ * Return: String length
+ **/
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return(i);
+}
