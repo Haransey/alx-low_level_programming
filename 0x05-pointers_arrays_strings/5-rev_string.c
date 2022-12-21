d875198827ff678cd99ec34aaae53a22b40a8120
Haransey <adeyemiyakub93@gmail.com>
2022-12-20 17:55:45 -0800

    ex06

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,10 +16,14 @@ void rev_string(char *s)
 	while (s[c] != '\0')
 		c++;
 	for (k = 1; k < c; k++)
+	{
 		a++;
+	}
 	for (i = 0; i < (c / 2); i++)
+	{
 		x = s[i];
 		s[i] = *a;
 		*a = x;
 		a--;
+	}
 }
