d866cc0e6271954eeb868be2898afe5948269aae
Haransey <adeyemiyakub93@gmail.com>
2022-12-22 00:06:31 -0800

    ex100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rot13 - rotate characters 13 places in the alphabet
+ * @s: string
+ * Return: string `s` rotated
+ */
+
+char *rot13(char *s)
+{
+	int i;
+
+	i = 0;
+	while( s[i] != '\0')
+	{
+		if((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm') )
+			s[i] += 13;
+		else if((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
+			s[i] -= 13;
+		i++;
+	}
+	return (s);
+}
