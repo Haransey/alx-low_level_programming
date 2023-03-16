6723fe4e1ba161c546e27ff9d9e735098fa5566a
Haransey <adeyemiyakub93@gmail.com>
2023-03-16 14:19:58 -0700

    double singly link

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,12 +3,12 @@
 #include "lists.h"
 
 /**
-* *insert_dnodeint_at_index - that inserts a new node at a given position
-* @h: variable pointer
-* @idx: index
-* @n: value
-* Return: the address of the new node, or NULL if it failed
-*/
+ * insert_dnodeint_at_idx - insert a new node at given position
+ * @h: double pointer to head
+ * @idx: index to insert into
+ * @n: value to store in new node
+ * Return: Address of new node, or NULL if failed
+ */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
